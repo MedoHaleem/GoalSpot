1ff7db3b3a93c7b7dbd0826a0d6431da8333b785
Medo <medo.a.haleem@gmail.com>
2019-07-03 15:03:23 +0200

    adding User Model

---
+++ guessing_game.ex
@@ -0,0 +1,3 @@
+defmodule GuessingGame do
+
+end
---
+++ lib/goalspot/goal/password.ex
@@ -0,0 +1,6 @@
+defmodule Goalspot.Password do
+  import Pbkdf2, only: [hash_pwd_salt: 1]
+  def hash(password) do
+    hash_pwd_salt(password)
+  end
+end
